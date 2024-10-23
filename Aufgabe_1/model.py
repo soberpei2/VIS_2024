1d566f29f62df0cd5c4199dde2a6e84dbb8af2e5
fuerber <bernhardfuerlinger@gmail.com>
2024-10-23 22:11:45 +0200

    implementation of the dydt

--- Aufgabe_1/model.py
+++ Aufgabe_1/model.py
@@ -18,18 +18,24 @@ class Model:
 class SingleMassOscillator(Model):
     def __init__(self, iniState, m, k, d):
         super().__init__(iniState)
+        
         #your implementation here
+        self.__m__ = m
+        self.__k__ = k
+        self.__d__ = 2 * d * np.sqrt(m * k)
 
+    
     def dydt(self, t):
         """Compute the derivatives of the state (velocity and acceleration)."""
         #----------------------------------------------------------------------------
         #  | implementation here  |
         # \ /                    \ /
         #  v                      v
-        position = 0.
-        velocity = 0.
-        acceleration = 0.        
-        
+        position = self.get_state[0]
+        velocity = self.get_state[1]
+
+        acceleration = -(self.__k__ * position - self.__d__ * velocity) / self.__m__
+
         return np.array([velocity, acceleration]) 
         #----------------------------------------------------------------------------
 
