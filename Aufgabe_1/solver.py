1ed49ca27d8d7745bbf3c69101054a88fc1f0e56
fuerber <bernhardfuerlinger@gmail.com>
2024-10-23 22:34:11 +0200

    implementation of implicitSolver

--- Aufgabe_1/solver.py
+++ Aufgabe_1/solver.py
@@ -1,4 +1,5 @@
 import model
+import numpy as np
 
 class Solver:
     def __init__(self, model2Solve):
@@ -16,7 +17,7 @@ class SolverExplicit(Solver):
 
     def step(self, t, dt):
         
-        """Perform one step of numerical integration."""
+        """Perform one step of numerical integration using forward Euler"""
         #----------------------------------------------------------------------------
         #  | implementation here  |
         # \ /                    \ /
@@ -24,4 +25,27 @@ class SolverExplicit(Solver):
         #----------------------------------------------------------------------------
         current_state = self.__model__.get_state()
         new_state = current_state + dt * self.__model__.dydt(t)
+        self.__model__.set_state(new_state)
+
+class SolverImplicit(Solver):
+    def __init__(self, model2Solve, tolerance=1e-6, max_iterations=100):
+        super().__init__(model2Solve)
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
+
+    def step(self, t, dt):
+        """Perform one step of implicit numerical integration using backward Euler."""
+        current_state = self.__model__.get_state()
+        new_state = current_state
+
+        for _ in range(self.max_iterations):
+            new_state_next = current_state + dt * self.__model__.dydt(t + dt)
+            
+            # Check for convergence
+            if np.linalg.norm(new_state_next - new_state) < self.tolerance:
+                new_state = new_state_next
+                break
+            
+            new_state = new_state_next
+        
         self.__model__.set_state(new_state)
