440710a5bf685581fed741d2845ed3d80dde01ac
Stefan Oberpeilsteiner <95639684+soberpe@users.noreply.github.com>
2024-10-25 12:57:45 +0200

    my working solver + model (#3)
    Co-authored-by: soberpe <stefan@principia-mbs.com>

--- Aufgabe_1/solver.py
+++ Aufgabe_1/solver.py
@@ -15,12 +15,37 @@ class SolverExplicit(Solver):
         super().__init__(model2Solve)
 
     def step(self, t, dt):
+        """Perform one step of numerical integration (e.g., explicit Euler)."""
+        current_state = self.__model__.get_state()
         
-        """Perform one step of numerical integration."""
-        #----------------------------------------------------------------------------
-        #  | implementation here  |
-        # \ /                    \ /
-        #  v                      v
+        # Call the model's dydt method to get the derivatives of the state
+        derivatives = self.__model__.dydt(t)
         
-        #----------------------------------------------------------------------------
-        
+        # Update the state using the Euler method
+        new_state = current_state + derivatives * dt
+        self.__model__.set_state(new_state)
+
+class SolverImplicit(Solver):
+    def __init__(self, model2Solve):
+        super().__init__(model2Solve)
+
+    def step(self, t, dt):
+        """Perform one step of implicit integration (e.g., implicit Euler)."""
+        current_state = self.__model__.get_state()
+
+        def implicit_function(new_state):
+            """Implicit update function."""
+            # Set the state in the model to new_state temporarily
+            self.__model__.set_state(new_state)
+            # Compute dydt at the new state and return the implicit function result
+            return new_state - (current_state + self.__model__.dydt(t + dt) * dt)
+
+        # Initial guess for the new state (using the current state)
+        new_state_guess = current_state.copy()
+
+        # Fixed-point iteration to solve for the new state
+        for _ in range(10):
+            new_state_guess = new_state_guess - 0.5 * implicit_function(new_state_guess)
+
+        # Set the final guessed state in the model
+        self.__model__.set_state(new_state_guess)
