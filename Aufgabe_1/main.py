b25f9fb5b6301e057f7cd8534857f798a0784a4f
HaHummer <hannes.hummer@students.fh-wels.at>
2024-10-26 19:50:35 +0200

    neuaufbau von Main  zum Verständnis

--- Aufgabe_1/main.py
+++ Aufgabe_1/main.py
@@ -1,42 +1,48 @@
 # main.py
 
+#importieren von Bibliotheken
 import numpy as np
 import matplotlib.pyplot as plt
+
+#Verbindung herstellen zu model und solver durch importieren
 import model
 import solver
 
+# Funktion für durchlauf der Funktion
 def run_simulation():
-    # Parameters
-    m = 1.0      # mass (kg)
-    k = 10000.0     # stiffness (N/m)
-    d = 0.001      # damping coefficient
+    #definieren von Parametern
+    m = 1.0     #Masse (kg)
+    k = 1000    # Steifigkeit (N/m) 10000 lt Angabe
+    d = 0.001   # Dämpfungskoeff.
 
-    # initial conditions
-    iniStates = np.array([0.0, 0.0])
+    #Initialisierungs-Vektor
+    iniStates = np.array([0.0,3.0])
 
-    # Time parameters
-    t_final = 10.0
-    dt = 0.001
-    num_steps = int(t_final / dt)
+    #Zeit Parameter
+    t_final = 10.0          #10 sec bis ende Rechnung
+    dt = 0.001              #Rechenschritt
+    num_steps = int(t_final/dt)  # Anzahl der rechenschritte
 
-    # Create a model (SingleMassOscillator)
-    myModel = model.SingleMassOscillator(iniStates, m, k, d)
+    # erstellen eines Modells (in diesem Fall 1 Massenschwinger)
 
-    # Create a solver
-    mySolver = solver.SolverExplicit()
+    myModel = model.SingleMassOscillator(iniStates,m,k,d) #instanziert Objekt myModel von der Klasse SingleMassOscillator welches sich unter model befindet
 
-    # Arrays to store time and position for plotting
-    times = np.linspace(0, t_final, num_steps)
-    positions = np.zeros_like(times)
+    # erstellen/Wahl eines Solvers mit Eingang des zu lösenden Models
+    mySolver = solver.SolverExplicit(myModel)
 
-    # Simulation loop
-    for step in range(num_steps):
-        t = step * dt
-        # Store the current position for plotting
-        positions[step] = myModel.get_state()[0]
-        
-        # Take a time step
-        mySolver.step(t, model, dt)
+    # Array für das speichern von Zeit und Position für das Plotten
+    times = np.linspace(0,t_final,num_steps)
+    positions = np.zeros_like(times)            #gleich viele Stellen wie array times
+
+    #Simulations Schleife
+
+    for step in range(num_steps):       #erhöhen von stepum 1
+        t = step*dt                     #aktuelle Sim-Zeit
+        #speichern der Position
+        positions[step]=myModel.get_state()[0] #nimmt erstes Element (Weg) von Model
+
+        #gehen eines Zeitschrittes
+        mySolver.step(t,myModel,dt)
 
     # Plotting the result
     plt.plot(times, positions)
