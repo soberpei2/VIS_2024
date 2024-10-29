corrected eq of motion and d coefficient calculation

Aufgabe_1/main.py
@@ -9,7 +9,7 @@ def run_simulation():
     # Parameters
     m = 1.0      # mass (kg)
     k = 100.0     # stiffness (N/m)
-    d = 0.01      # damping coefficient
+    d = 0.01      # damping coefficient D
 
     # initial conditions
     iniStates = np.array([1.0, 0.0])

Aufgabe_1/model.py
@@ -8,8 +8,8 @@ class Model:
         return self.state
     
     def set_state(self, new_state):
-        self.state = new_state
-
+        self.state = new_state # new_state wird am Ende vom Solverschritt gesetzt 
+        
     def dydt(self, t, y):
         """Defines the system's dynamics: computes the derivative of the state vector."""
         raise NotImplementedError("This method should be implemented by subclasses.")
@@ -18,20 +18,16 @@ class Model:
 class SingleMassOscillator(Model):
     def __init__(self, iniState, m, k, d):
         super().__init__(iniState)
-        self.m = m #Masse
-        self.k = k #Steifigkeit
-        self.d = 2*d*np.sqrt(self.k/self.m) #Umrechnen von D auf d
+        self.__m__ = m #Masse
+        self.__k__ = k #Steifigkeit
+        self.__d__ = 2*d*np.sqrt(self.__m__*self.__k__) #Umrechnen von D auf d
 
     def dydt(self, t):
         """Compute the derivatives of the state (velocity and acceleration)."""
         position = self.state[0]
         velocity = self.state[1]
-        
-        #position = 0.
-        #velocity = 0.
 
-        acceleration = (-self.k * position - self.d * velocity) 
+        # aus Bewegungsgleichung
+        acceleration = (-self.__k__ * position - self.__d__ * velocity)/self.__m__
         
-        return np.array([velocity, acceleration]) 
-        #----------------------------------------------------------------------------
-
+        return np.array([velocity, acceleration])