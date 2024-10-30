c04a7265972e063ceb31d7b327d6fecb5619fa88
alexB126 <alexander.bloechl@students.fh-wels.at>
2024-10-30 14:23:42 +0100

    neue bib

--- Aufgabe_1/main.py
+++ Aufgabe_1/main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import xlsxwriter
 
 # Enable LaTeX in matplotlib
-plt.rcParams['text.usetex'] = True
+# plt.rcParams['text.usetex'] = True
 
 import model
 import solver
