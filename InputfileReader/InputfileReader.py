88764e81481a53829ed4318bbad5fe89eff45e0e
LukasLeberbauer999 <s2310566013@fhooe.at>
2024-11-06 17:01:18 +0100

    Test 06.11

---
+++ InputfileReader/InputfileReader.py
@@ -0,0 +1,28 @@
+import mbsObject
+
+f = open("InputfileReader/test.fdd","r")
+
+fileContent = f.read().splitlines()     #was auf der Festplatte ist kommt in den Arbeitsspeicher
+f.close()
+
+currentBlockType = ""
+currentTextBlock = []
+listofmbsObjects = []
+search4Objects = ["RIGID_BODY", "CONSTRAINT"]
+
+for line in fileContent:                #für alle Zeilen
+    if(line.find("$")>=0):              #neuer Block gefunden
+        if(currentBlockType != ""):
+            if(currentBlockType == "RIGID_BODY"):
+                listofmbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+            currentBlockType = ""
+            
+    for type_i in search4Objects:
+        if(line.find(type_i,1,len(type_i)+1) >= 0):
+            currentBlockType = type_i
+            currentTextBlock.clear()
+            break
+
+    currentTextBlock.append(line)
+
+print("List of mbsObjects = ",listofmbsObjects)
