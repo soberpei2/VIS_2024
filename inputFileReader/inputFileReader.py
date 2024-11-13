2a4dbf2071fb5993054b14f3778d04277fb5ef0d
fuerber <bernhardfuerlinger@gmail.com>
2024-11-13 10:13:17 +0100

    start of VIS3UE 20241113

--- inputFileReader/inputFileReader.py
+++ inputFileReader/inputFileReader.py
@@ -1,13 +1,32 @@
 
+import mbsObject
+
 f = open("inputFileReader/test.fdd","r")
 
 fileContent = f.read().splitlines()
 f.close()
 
 numOfRigidBodies = 0
+numOfConstraints = 0
+currentBlockType = ""
+currentTextBlock = []
+search4Objects = ["RIGID_BODY", "CONSTRAINT"]
+listOfMbsObjects = []
+
+#factoryFunction
 for line in fileContent:
     if(line.find("$") >= 0): #found new block
-        if(line.find("RIGID_BODY",1,len("RIGID_BODY")+1) >= 0):
-            numOfRigidBodies += 1
+        if(currentBlockType != ""):
+            if(currentBlockType == "RIGID_BODY"):
+                listOfMbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+            currentBlockType = ""
+    
+    for type_i in search4Objects:
+        if(line.find(type_i,1,len(type_i)+1) >= 0):
+            currentBlockType = type_i
+            currentTextBlock.clear()
+            break
 
-print(numOfRigidBodies)
+    currentTextBlock.append(line)
+
+print(len(listOfMbsObjects))
