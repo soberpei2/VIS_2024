21c3cf6f08ed2c79920350eda03baa9f6fc9240c
fpointin <S2310566016@fhooe.at>
2024-11-13 11:09:29 +0100

    added rigid body class and float/vector handling

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -7,25 +7,26 @@ fileContent = f.read().splitlines()         # das was auf der Festplatte steht k
 f.close                                     # sauber arbeiten
 
 
-numOfRigidBodies = 0                        # Variablen anlegen
-numOfConstraints = 0
+#numOfRigidBodies = 0                        # Variablen anlegen
+#numOfConstraints = 0
 currentBlockType = ""
 currentTextBlock = []
-search4Objects = ["RIGID_BODY","CONSTRAINT"]
 listOfMbsObjects = []
+search4Objects = ["RIGID_BODY","CONSTRAINT"]    #mögliche Schlagwörter
 
 
 for line in fileContent:                    # für alle Zeilen (line ist Iterator, alle Zeilen von Filecontent)
     if(line.find("$") >= 0):                # im fdd file ist $ immer das "Schlagwort"
-        if(currentBlockType != ""):
+        if(currentBlockType != ""):         # hier wird mit jedem Schlagwort einzeln gearbeitet, dass verschiedene Konstruktoren angewendet werden
             if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
             currentBlockType = ""
 
-    for type_i in search4Objects:
+    for type_i in search4Objects:                       # Suche über alle Schlagwörter
         if(line.find(type_i,1,len(type_i)+1) >= 0):     # Position 0 ist der $, von 1 bis Länge+1 muss das RIGID_BODY stehen
             currentBlockType = type_i
             currentTextBlock.clear()
+            break
 
     currentTextBlock.append(line)
 
--- inputfilereader/mbsObject.py
+++ inputfilereader/mbsObject.py
@@ -1,8 +1,33 @@
 class mbsObject:
-    def __init__(self,type,text):
+    def __init__(self,type,text,parameter):
         self.__type = type
-        
+        self.parameter = parameter  #self.parameter ist membervariable, der wird referenz auf parameter zugewiesen
+                                    # es ist also egal, ob man self.parameter oder parameter schreibt
+
         for line in text:
             splitted = line.split(":")
-            if(splitted[0].strip() == "mass"): # strip = alle unnötigen Leerzeichen weg
-                self.mass = float(splitted[1])
+            for key in parameter.keys(): #über dictionary drüber suchen... parameter.keys() = mass und COG
+                if(splitted[0].strip() == key):          # strip = alle unnötigen Leerzeichen weg
+                    if(parameter[key]["type"] == "float"):                          # Behandlung, wenn key ein float ist
+                        parameter[key]["value"] = self.str2float(splitted[1])
+                    elif(parameter[key]["type"] == "vector"):                       # Behandlung, wenn key ein vector ist
+                        parameter[key]["value"] = self.str2vector(splitted[1])
+
+
+    def str2float(self,inString,):  # string in Float konvertieren
+        return float(inString)
+    
+    def str2vector(self,inString,): # string in Vektor umbasteln
+        return [float(inString.split(",")[0]),float(inString.split(",")[1]),float(inString.split(",")[2])]
+
+
+#in den Unterklassen wird dann nach den Eigenschaften gesucht
+
+class rigidBody(mbsObject):
+    def __init__(self,text):                            #type brauchen wir nicht, weil ich bin ein Rigid Body
+        parameter = {                                   #dictionary (key & value) mit den parametern
+            "mass": {"type": "float", "value": 1.},     #sollte die Masse nicht gefunden werden, wird der default Wert 1 gesetzt bleiben
+            "COG": {"type": "vector", "value": [1.,1.,1.]}
+        } 
+
+        mbsObject.__init__(self,"rigidBody",text,parameter)       #wenn man es so aufruft, braucht man auch das init
