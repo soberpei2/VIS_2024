7cafa0a5160c05e48c187f376c467de1296d130c
alexB126 <alexander.bloechl@students.fh-wels.at>
2024-11-13 12:17:44 +0100

    wieUnterricht1113

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -1,5 +1,6 @@
 
 import mbsObject
+import json
 
 f = open("inputfilereader/test.fdd","r")
 
@@ -14,7 +15,7 @@ for line in fileContent:
     if(line.find("$") >= 0): #new block found 
         if(currentBlockType != ""):
             if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
             currentBlockType = ""
 
     for type_i in search4Objects: #sucht nach Objekten und speichert diese
@@ -24,5 +25,23 @@ for line in fileContent:
             break
 
     currentTextBlock.append(line)
+
+# schreibt alles um auf ein json file 
+modelObjects = []
+for object in listOfMbsObjects:
+    modelObjects.append(object.parameter)
+jDataBase = json.dumps({"modelObjects": modelObjects})
+with open("inputfilereader/test.json","w") as outfile:      # mit alt + shift + f zum strukturieren
+    outfile.write(jDataBase)
+
+f = open("inputfilereader/test.json","r")   # r für read
+data = json.load(f)
+f.close()
+
+
+fds = open("inputfilereader/test.fds","w") # w ... write | a für append wenn es vorhanden ist
+for mbsObject_i in listOfMbsObjects:
+    mbsObject_i.writeInputfile(fds)
+fds.close()
 
 print(len(listOfMbsObjects))
