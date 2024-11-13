1c880bfdf792d5cb37835f48690a348f075b5176
alexweissFH <S2310566021@fhooe.at>
2024-11-13 12:18:52 +0100

    jason ergänzt

--- Inputfilereader/Inputfilereader.py
+++ Inputfilereader/Inputfilereader.py
@@ -1,5 +1,6 @@
 
 import mbsObject
+import json
 
 f = open("inputfilereader/test.fdd","r")
 
@@ -11,31 +12,45 @@ numOfConstraints = 0
 currentBlockType = ""
 currentTextBlock = []
 listOfMbsObjects = []
-search4Objects = ["RIGID_BODY","CONSTRAINT"]
-for line in fileContent:
-    if(line.find("$")>=0):#new block found
+search4Objects = ["RIGID_BODY","CONSTRAINT"]#mögliche Schlagwörter
+for line in fileContent:#Für alle Zeilen (line ist Iterator, alle Zeilen von Filevonten)
+    if(line.find("$")>=0):#new block found, im fdd file ist immer das Schlagwort
         if(currentBlockType !=""):
             if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+                listOfMbsObjects.append(mbsObject.rigidbody(currentTextBlock))
             #elif(currentBlockType == "CONSTRAINT"):
                 #numOfConstraints += 1
             currentBlockType = ""
 
 
-    for type_i in search4Objects:
+    for type_i in search4Objects:#Suche für alle Schlagwörter
         if(line.find(type_i,1,len(type_i)+1)>=0):
             currentBlockType = type_i
             currentTextBlock.clear()
 
             currentTextBlock.append(line)
 
+modelObjects = []
+for object in listOfMbsObjects:
+    modelObjects.append(object.parameter)
+jDataBase = json.dumps({"modelObjects": modelObjects})
+with open ("inputfilereader/test.json","w") as outfile:
+    outfile.write(jDataBase)
 
+f=open("inputfilereader/test.json","r")
+data=json.load(f)
+f.close()
     #if(line.find("RIGID_BODY",1,len("RIGID_BODY")+1)>= 0):
         #currentBlockType = "RIGID_BODY"
        # currentTextBlock.clear()
        # break
 
-    currentTextBlock.append(line)
+#currentTextBlock.append(line)
+
+fds = open("inputfilereader/test.fds","w")
+for mbsObject_i in listOfMbsObjects:
+    mbsObject_i.writeInputfile(fds)
+fds.close()
 print(len(listOfMbsObjects))
 
 
