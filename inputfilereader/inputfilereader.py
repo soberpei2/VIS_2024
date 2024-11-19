e83fabb2f8feb86779375f2a689681140e8ab797
soberpe <stefan@principia-mbs.com>
2024-11-19 08:36:16 +0100

    added functionality for reading/creating rigid bodies

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -1,5 +1,6 @@
 
 import mbsObject
+import json
 
 f = open("inputfilereader/test.fdd","r")
 
@@ -14,7 +15,7 @@ for line in fileContent:
     if(line.find("$") >= 0):#new block found
         if(currentBlockType != ""):
             if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.mbsObject("body",currentTextBlock))
+                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
             currentBlockType = ""
 
     for type_i in search4Objects:
@@ -26,3 +27,22 @@ for line in fileContent:
     currentTextBlock.append(line)
 
 print(len(listOfMbsObjects))
+
+
+#import/export functionality (for later use in model.py)
+modelObjects = []
+for object in listOfMbsObjects:
+    modelObjects.append(object.parameter)
+jDataBase = json.dumps({"modelObjects": modelObjects})
+with open("inputfilereader/test.json", "w") as outfile:
+    outfile.write(jDataBase)
+
+f = open("inputfilereader/test.json","r")
+data = json.load(f)
+f.close()
+
+fds = open("inputfilereader/test.fds","w")
+for mbsObject_i in listOfMbsObjects:
+    mbsObject_i.writeInputfile(fds)
+fds.close()
+#-------------------------------------------------------
