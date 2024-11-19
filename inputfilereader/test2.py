11438a5fb935b68c5299f4c40feeaf0a655f5c84
alexB126 <alexander.bloechl@students.fh-wels.at>
2024-11-19 16:39:07 +0100

    Testen

--- inputfilereader/test2.py
+++ inputfilereader/test2.py
@@ -1,4 +1,31 @@
 import inputfilereader
+import vtk
 
+#listOfMyObjects = inputfilereader.readInput4Output("inputfilereader/test.fdd","inputfilereader/test1.json","inputfilereader/test1.fds")
 
-listOfMyObjects = inputfilereader.readInput4Output("inputfilereader/test.fdd","inputfilereader/test1.json","inputfilereader/test1.fds")
+cone = vtk.vtkConeSource() 
+cone.SetHeight(3.0) 
+cone.SetRadius(1.0) 
+cone.SetResolution(6)
+
+coneMapper = vtk.vtkPolyDataMapper() 
+coneMapper.SetInputConnection(cone.GetOutputPort())
+
+coneActor = vtk.vtkActor() 
+coneActor.SetMapper(coneMapper)
+
+ren1 = vtk.vtkRenderer() 
+ren1.AddActor(coneActor) 
+ren1.SetBackground(0.1, 0.2, 0.4)
+
+renWin = vtk.vtkRenderWindow() 
+renWin.AddRenderer(ren1) 
+renWin.SetSize(300, 300)
+
+for i in range(360): 
+    renWin.Render() 
+    ren1.GetActiveCamera().Azimuth(1)
+
+iren = vtk.vtkRenderWindowInteractor() 
+iren.SetRenderWindow(renWin) 
+iren.Start()
