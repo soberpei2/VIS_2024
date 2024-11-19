08a2f3b26cb03273478795e901f57a0dbc6756bc
alexB126 <alexander.bloechl@students.fh-wels.at>
2024-11-19 16:20:17 +0100

    verbesserungenOderauchNicht

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -2,45 +2,48 @@
 import mbsObject
 import json
 
-f = open("inputfilereader/test.fdd","r")
+def readInput4Output(inputfilepath, jsonfilepath, fdsfilepath):
+    # Dateiinhalt lesen
+    with open(inputfilepath, "r") as f:
+        fileContent = f.read().splitlines()
 
-fileContent = f.read().splitlines()
-f.close()
 
-currentBlockType = ""
-currentTextBlock = []
-listOfMbsObjects = []
-search4Objects = ["RIGID_BODY", "CONSTRAINT"]
-for line in fileContent:
-    if(line.find("$") >= 0): #new block found 
-        if(currentBlockType != ""):
-            if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
-            currentBlockType = ""
+    currentBlockType = ""
+    currentTextBlock = []
+    listOfMbsObjects = []
+    # Wörter nach denen in der Liste "search4Objects" gesucht wird
 
-    for type_i in search4Objects: #sucht nach Objekten und speichert diese
-        if(line.find(type_i,1,len(type_i)+1) >= 0):
-            currentBlockType = type_i
-            currentTextBlock.clear()
-            break
+    search4Objects = ["RIGID_BODY", "CONSTRAINT"]
+    for line in fileContent:
+        if(line.find("$") >= 0): #new block found 
+            if(currentBlockType != ""):
+                if(currentBlockType == "RIGID_BODY"):
+                    listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
+                currentBlockType = ""
 
-    currentTextBlock.append(line)
+        for type_i in search4Objects: #sucht nach Objekten und speichert diese
+            if(line.find(type_i,1,len(type_i)+1) >= 0):
+                currentBlockType = type_i
+                currentTextBlock.clear()
+                break
 
-# schreibt alles um auf ein json file 
-modelObjects = []
-for object in listOfMbsObjects:
-    modelObjects.append(object.parameter)
-jDataBase = json.dumps({"modelObjects": modelObjects})
-with open("inputfilereader/test.json","w") as outfile:      # mit alt + shift + f zum strukturieren
-    outfile.write(jDataBase)
+        currentTextBlock.append(line)
 
-f = open("inputfilereader/test.json","r")   # r für read
-data = json.load(f)
-f.close()
+    # schreibt alles um auf ein json file 
+    modelObjects = []
+    for object in listOfMbsObjects:
+        modelObjects.append(object.parameter)
+    jDataBase = json.dumps({"modelObjects": modelObjects})
 
-fds = open("inputfilereader/test.fds","w") # w ... write | a für append wenn es vorhanden ist
-for mbsObject_i in listOfMbsObjects:
-    mbsObject_i.writeInputfile(fds)
-fds.close()
+    with open(jsonfilepath, "w") as outfile:
+        outfile.write(jDataBase)
 
-print(len(listOfMbsObjects))
+    with open(jsonfilepath, "r") as jf:
+        data = json.load(jf)
+
+ # FDS-Datei schreiben
+    with open(fdsfilepath, "w") as fds:
+        for mbsObject_i in listOfMbsObjects:
+            mbsObject_i.writeInputfile(fds)
+        
+    return listOfMbsObjects
---
+++ inputfilereader/test2.py
@@ -0,0 +1,4 @@
+import inputfilereader
+
+
+listOfMyObjects = inputfilereader.readInput4Output("inputfilereader/test.fdd","inputfilereader/test1.json","inputfilereader/test1.fds")
