14640f0fb50ac8e7eccecbc1ce949c0798e5062e
alexweissFH <S2310566021@fhooe.at>
2024-11-20 10:54:46 +0100

    neue Ordnerstruktur

---
+++ Aufgabe 2/Inputfilereader/Inputfilereader.py
@@ -0,0 +1,57 @@
+
+import mbsObject
+import json
+import vtk
+
+f = open("inputfilereader/test.fdd","r")
+
+fileContent = f.read().splitlines()
+f.close()
+
+numOfRigidBodies = 0
+numOfConstraints = 0
+currentBlockType = ""
+currentTextBlock = []
+listOfMbsObjects = []
+search4Objects = ["RIGID_BODY","CONSTRAINT"]#mögliche Schlagwörter
+for line in fileContent:#Für alle Zeilen (line ist Iterator, alle Zeilen von Filevonten)
+    if(line.find("$")>=0):#new block found, im fdd file ist immer das Schlagwort
+        if(currentBlockType !=""):
+            if(currentBlockType == "RIGID_BODY"):
+                listOfMbsObjects.append(mbsObject.rigidbody(currentTextBlock))
+            #elif(currentBlockType == "CONSTRAINT"):
+                #numOfConstraints += 1
+            currentBlockType = ""
+
+
+    for type_i in search4Objects:#Suche für alle Schlagwörter
+        if(line.find(type_i,1,len(type_i)+1)>=0):
+            currentBlockType = type_i
+            currentTextBlock.clear()
+
+            currentTextBlock.append(line)
+
+modelObjects = []
+for object in listOfMbsObjects:
+    modelObjects.append(object.parameter)
+jDataBase = json.dumps({"modelObjects": modelObjects})
+with open ("inputfilereader/test.json","w") as outfile:
+    outfile.write(jDataBase)
+
+f=open("inputfilereader/test.json","r")
+data=json.load(f)
+f.close()
+    #if(line.find("RIGID_BODY",1,len("RIGID_BODY")+1)>= 0):
+        #currentBlockType = "RIGID_BODY"
+       # currentTextBlock.clear()
+       # break
+
+#currentTextBlock.append(line)
+
+fds = open("inputfilereader/test.fds","w")
+for mbsObject_i in listOfMbsObjects:
+    mbsObject_i.writeInputfile(fds)
+fds.close()
+print(len(listOfMbsObjects))
+
+
