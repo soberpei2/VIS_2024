e58ba23245bc913f93618c9bd0e1d65542cf8bf0
mariostein <s2310566020@fhooe.at>
2024-11-20 13:04:55 +0100

    Kommentieren und verstehen

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -1,41 +1,46 @@
 
+
 import mbsObject
 import json
 
-f = open("inputfilereader/test.fdd","r")
+f = open("inputfilereader/test.fdd","r")    #Öffnen des .fdd formates (dort steht der shit auf Freedyn)
 
-fileContent = f.read().splitlines()
-f.close()
+fileContent = f.read().splitlines()         #Das file ist in einer Wust, hier wird es in Zeilen formatiert und Abgespeichert
+f.close()                                   #Schließen das das Programm wieder benutzt werden kann (von anderen Programmen)
 
-currentBlockType = ""
-currentTextBlock = []
-listOfMbsObjects = []
-search4Objects  = ["RIGID_BODY", "CONSTRAINT"]
-for line in fileContent:
-    if(line.find("$") >= 0):#new block found
-        if(currentBlockType != ""):
-            if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
-            currentBlockType = ""
+currentBlockType = ""                       #Anlegen einer leeren Variable mit dem Namen currentBlockType (jetztiger Blocktyp)
+currentTextBlock = []                       #Anlegen einer leeren Variable mit dem Namen currentTextBlock (Variablen des Types)
+listOfMbsObjects = []                       #Anlegen einer leeren Variable mit dem Namen listOfMbsObjects (wie viele Objekte gibt es)
+search4Objects  = ["RIGID_BODY", "CONSTRAINT"]  #Suchen nach Gruppen
+for line in fileContent:                        #gehe durch die Zeilen im gespeicherten .fdd files
+    if(line.find("$") >= 0):                    #Schauen ob eine  Dollarzeichen gefunden wird
+        if(currentBlockType != ""):                 #Webn der BlockType nicht leer ist gehe weiter
+            if(currentBlockType == "RIGID_BODY"):   #Wenn ein Rigid Body gefunden wird gehe weiter
+                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))  #Füge die Eigenschaften des Objectes zur Liste dazu
+            currentBlockType = ""                   #Leere den currentBlockType
 
-    for type_i in search4Objects:
-        if(line.find(type_i,1,len(type_i)+1) >= 0):
-            currentBlockType = type_i
-            currentTextBlock.clear()
+    for type_i in search4Objects:           #for-Schleife bis das if wahr ist, Suchen ob es sich um ein search4Object handel (z.B: Rigid_Body)
+        if(line.find(type_i,1,len(type_i)+1) >= 0):    #Suchen ob die Buchstaben zusammenpassen für ein search4Object 
+            currentBlockType = type_i       #Schreibe das Wort in currentBlockType
+            currentTextBlock.clear()        #TextBlock(Variablen) bereinigen
             break
     
-    currentTextBlock.append(line)
+    currentTextBlock.append(line)           #Speichere die Informationen der aktive Zeile in den currentTextBlock
 
-modelObjects =[]
-for object in listOfMbsObjects:
-    modelObjects.append(object.parameter)
-jDataBase = json.dumps({"modelObjects": modelObjects})
-with open("inputfilereader/test.json","w") as outfile:
-    outfile.write(jDataBase)
+modelObjects =[]                                #Anlegen einer leeren Variable mit dem Namen modelObjects
+for object in listOfMbsObjects:                 #gehe durch die Objecte in listOfObjects
+    modelObjects.append(object.parameter)       #Wir schreiben in modelObjects alle Parameter der Objecte (search4Objects)
+jDataBase = json.dumps({"modelObjects": modelObjects})  #Speichern der Daten in eine Variable (jDataBase) die wir für json brauchen
+with open("inputfilereader/test.json","w") as outfile:  #leeres json öffnen
+    outfile.write(jDataBase)                          #schreiben der Daten in json
 
-fds = open("inputfilereader/test.fds","w")
-for mbsObject_i in listOfMbsObjects:
-    mbsObject_i.writeInputfile(fds)
-fds.close()
+fj = open("inputfilereader/test.json","r")      #öffnen der test.json Datei mit leseberechtigung
+data = json.load(fj)                            #Warum?? data ist unverwendet
+fj.close()                                      #Speichern und Schließen
 
-print(len(listOfMbsObjects))
+fds = open("inputfilereader/test.fds","w")          #Öffnen eines fds files mit dem Namen test.fds mit Schreibberechtigung
+for mbsObject_i in listOfMbsObjects:                #for mit mbsObject als Counter in listOfMbsObjects
+    mbsObject_i.writeInputfile(fds)                 #Schreibe das fds file (Zeilenweise die Daten von listOfMbsObjects)
+fds.close()                                         #Speichern und Schließen
+
+print(len(listOfMbsObjects))                        #Schreiben der Objectanzahl auf die Konsole
--- inputfilereader/mbsObject.py
+++ inputfilereader/mbsObject.py
@@ -1,14 +1,14 @@
 
-class mbsObject:
-    def __init__(self, type, subtype, text, parameter):
-        self.__type = type
-        self.__subtype = subtype
-        self.parameter=parameter
+class mbsObject:                                            #Klasse erstellen
+    def __init__(self, type, subtype, text, parameter):     #Definieren (init -> Konstructor) ,sich selbst, 
+        self.__type = type                                  #pointer of sich selbt vom Typ type
+        self.__subtype = subtype                            #pointer of sich selbt vom Typ subtype
+        self.parameter = parameter                            #pointer of sich selbt vom Typ parameter
 
-        for line in text:
-            splitted = line.split(":")
-            for key in parameter.keys():
-                if(splitted[0].strip() == key):     #Wenn das gleich dem key ist dann befüllen wir das
+        for line in text:                                   #line ist counter im text
+            splitted = line.split(":")                      #legen die Varibale splitted an und Trenne die Spalte bei Doppelpunkt
+            for key in parameter.keys():                    #key ist counter in mbsObject.parameter
+                if(splitted[0].strip() == key):             
                     if(parameter[key]["type"] == "float"):
                         parameter[key]["value"] = self.str2float(splitted[1])
                     if(parameter[key]["type"] == "vector"):
@@ -42,7 +42,7 @@ class rigidBody(mbsObject):
     def __init__(self,text):
         parameter = {
             "mass": {"type": "float", "value": 1.},      #masse ist gesucht, float is datentyp, wert mit defaultvariable
-            "COG": {"type": "vector", "value":[0.,0.,0.]}       #keine ahnung was das ist
+            "COG": {"type": "vector", "value":[0.,0.,0.]}       #Center of Gravity ist gesucht, vector ist datentyp,
         }
 
         mbsObject.__init__(self,"body","Rigid_EulerParameter_PAI",text, parameter)
