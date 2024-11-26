eb05da3fcc601b581764b67199a40b7313b6f383
arnoberm <S2310566015@fhooe.at>
2024-11-26 14:22:55 +0100

    Test.fdd auf neue Version, beginn VTK

--- inputfilereader/inputfilereader.py
+++ inputfilereader/inputfilereader.py
@@ -1,44 +1,73 @@
-
-import mbsObject
 import json
+import mbsObject
 
+def read_input_file(filepath): #Liest eine Eingabedatei und gibt den Inhalt als Liste von Zeilen zurück.
+    with open(filepath, "r") as file:
+        f= file.read().splitlines()
+        f.append("$")        
+        
+        return f
 
-f = open("inputfilereader/test.fdd","r")
+def parse_file_content(file_content, search4Objects): #Parst den Inhalt einer Datei und gibt eine Liste von Objekten zurück.
+    currentBlockType = ""
+    currentTextBlock = []
+    listOfMbsObjects = []
+    
+    for line in file_content:
+        if "$" in line:  # Neuer Block gefunden
+            if(currentBlockType != ""):
+                if currentBlockType == "RIGID_BODY":
+                    listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
 
-fileContent = f.read().splitlines()
-f.close()
+                elif currentBlockType == "CONSTRAINT":
+                    listOfMbsObjects.append(mbsObject.constraint(currentTextBlock))
 
-currentBlockType = ""
-currentTextBlock = []
-listOfMbsObjects = []
-search4Objects  = ["RIGID_BODY", "CONSTRAINT"]
-for line in fileContent:
-    if(line.find("$") >= 0):#new block found
-        if(currentBlockType != ""):
-            if(currentBlockType == "RIGID_BODY"):
-                listOfMbsObjects.append(mbsObject.rigidBody(currentTextBlock))
+                elif currentBlockType == "SETTINGS":
+                    listOfMbsObjects.append(mbsObject.settings(currentTextBlock))
+                    
+                
             currentBlockType = ""
 
-    for type_i in search4Objects:
-        if(line.find(type_i,1,len(type_i)+1) >= 0):
-            currentBlockType = type_i
-            currentTextBlock.clear()
-            break
+        for type_i in search4Objects:
+            if line.find(type_i, 1, len(type_i) + 1) >= 0:
+                currentBlockType = type_i
+                currentTextBlock.clear()
+                break
+
+        currentTextBlock.append(line)
     
-    currentTextBlock.append(line)
+    return listOfMbsObjects
 
-modelObjects =[]
-for object in listOfMbsObjects:
-    modelObjects.append(object.parameter)
-jDataBase = json.dumps({"modelObjects":modelObjects})
-with open("imputfilereader/test.json","w") as outfile:
-    outfile.write(jDataBase)
+def write_json(filepath, model_objects): #Schreibt die Modellobjekte als JSON-Datei.
+    jDataBase = json.dumps({"modelObjects": model_objects}, indent=4)
+    with open(filepath, "w") as outfile:
+        outfile.write(jDataBase)
 
+def read_json(filepath): #Liest eine JSON-Datei und gibt die Daten zurück.
+    with open(filepath, "r") as file:
+        return json.load(file)
 
+def write_fds(filepath, list_of_mbs_objects): #Schreibt die Eingabedatei für das FDS-Format.
+    with open(filepath, "w") as fds:
+        for mbsObject_i in list_of_mbs_objects:
+            mbsObject_i.writeInputfile(fds)
 
-fds= open("inputfilereader/test.fds","w")
-for mbsObject_i in listOfMbsObjects:
-    mbsObject_i.writeInputfile(fds)
-fds.close()
+def process_file(input_filepath, json_filepath, fds_filepath, search4Objects): #Hauptfunktion zur Verarbeitung der Datei.
+    file_content = read_input_file(input_filepath)
+    list_of_mbs_objects = parse_file_content(file_content, search4Objects)
+    
+    model_objects = [obj.parameter for obj in list_of_mbs_objects]
+    write_json(json_filepath, model_objects)
+    write_fds(fds_filepath, list_of_mbs_objects)
+    
+    print(f"Anzahl der Objekte: {len(list_of_mbs_objects)}")
 
-print(len(listOfMbsObjects))
+# Hauptprogramm
+if __name__ == "__main__":
+    input_filepath = "inputfilereader/test.fdd"
+    json_filepath = "inputfilereader/test.json"
+    fds_filepath = "inputfilereader/test.fds"
+    search4Objects = ["RIGID_BODY", "CONSTRAINT","SETTINGS"]
+    
+    process_file(input_filepath, json_filepath, fds_filepath, search4Objects)
+
--- inputfilereader/mbsObject.py
+++ inputfilereader/mbsObject.py
@@ -13,6 +13,11 @@ class mbsObject:
                         parameter[key]["value"] = self.str2float(splitted[1])
                     elif(parameter[key]["type"] == "vector"):
                         parameter[key]["value"] = self.str2vector(splitted[1])
+                    elif(parameter[key]["type"] == "int"):
+                        parameter[key]["value"] = self.str2int(splitted[1])
+                    elif(parameter[key]["type"] == "intvec"):
+                        parameter[key]["value"] = self.str2intvec(splitted[1])
+
 
     def writeInputfile(self,file):
         text= []
@@ -21,8 +26,14 @@ class mbsObject:
             if(self.parameter[key]["type"]=="float"):
                 text.append("\t"+key+" = "+self.float2str(self.parameter[key]["value"])+"\n")
             
-            if(self.parameter[key]["type"]=="vector"):
+            elif(self.parameter[key]["type"]=="vector"):
                 text.append("\t"+key+" = "+self.vector2str(self.parameter[key]["value"])+"\n")
+
+            elif(self.parameter[key]["type"]=="int"):
+                text.append("\t"+key+" = "+self.int2str(self.parameter[key]["value"])+"\n")
+
+            elif(self.parameter[key]["type"]=="intvec"):
+                text.append("\t"+key+" = "+self.intvec2str(self.parameter[key]["value"])+"\n")
         text.append("End"+ self.__type+"\n%\n")
 
         file.writelines(text)
@@ -33,18 +44,59 @@ class mbsObject:
     def float2str(Self,inFloat):
         return str(inFloat)
     
+    def str2int(self,inInt):
+        return float(inInt)
+    
+    def int2str(Self,inInt):
+        return str(inInt)
+    
                   
     def str2vector(self,inString):
         return [float(inString.split(",")[0]),float(inString.split(",")[1]),float(inString.split(",")[2])]
     
     def vector2str(self,inVector):
         return str(inVector[0]) + "," + str(inVector[1]) + "," + str(inVector[2])
+     
+    
+    def str2intvec(self,inIntvec):
+        return [int (inIntvec.split()[0]),int (inIntvec.split()[1]),int (inIntvec.split()[2])]
+    
+    def intvec2str(self,inIntvec):
+        return str(inIntvec[0]) + " " + str(inIntvec[1]) + " " + str(inIntvec[2])
     
 class rigidBody(mbsObject):
     def __init__(self,text):
         parameter = {  #Dictionary
             "mass": {"type": "float", "value": 1.},   #mass wird gesucht, float ist datentyp, value wird beschrieben, 1. = defaulkt value falls keine masse gefunden wird
-            "COG": {"type": "vector", "value": [0.,0.,0.]} #Center of Gravity
+            "COG": {"type": "vector", "value": [0.,0.,0.]}, #Center of Gravity
+            "position": {"type": "vector", "value": [0.,0.,0.]},
+            "color": {"type": "intvec", "value": [0.,0.,0.]},
+            "transparency": {"type": "float", "value": 1.}
         }
 
         mbsObject.__init__(self,"Body","RIGID_EulerParamter_PAI",text, parameter) #super... wäre auch möglich gewesen
+
+class constraint(mbsObject):
+    def __init__(self,text):
+        parameter = {  #Dictionary 
+            "position": {"type": "vector", "value": [0.,0.,0.]}, #Positionsvektor
+            "dx": {"type": "int", "value": 1.},
+            "dy": {"type": "int", "value": 1.},
+            "dz": {"type": "int", "value": 1.},
+            "ax": {"type": "int", "value": 1.},
+            "ay": {"type": "int", "value": 1.},
+            "az": {"type": "int", "value": 1.},
+        }
+
+        mbsObject.__init__(self,"constraint","constraint_fixed",text, parameter)
+
+class settings(mbsObject):
+    def __init__(self,text):
+        parameter = {  #Dictionary
+            "constraint icon scale": {"type": "float", "value": 1.},
+            "COG marker scale": {"type": "float", "value": 1.}, 
+            "gravity_vector": {"type": "vector", "value": [0.,0.,0.]}, 
+            "background color": {"type": "intvec", "value": [0.,0.,0.]}
+        }
+
+        mbsObject.__init__(self,"settings","settings_fixed",text, parameter)
