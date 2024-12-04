c198176e5ad857bd23c9ad2eed2f31bc04d0b9d6
alexweissFH <S2310566021@fhooe.at>
2024-12-04 15:22:30 +0100

    Tutorial fertig

---
+++ QT_Tutorial/main.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import sys
+import argparse
+import pandas as pd
+
+from PySide6.QtCore import QDateTime, QTimeZone
+from PySide6.QtWidgets import QApplication
+from main_window import MainWindow
+from main_widget import Widget
+
+
+def transform_date(utc, timezone=None):
+    utc_fmt = "yyyy-MM-ddTHH:mm:ss.zzzZ"
+    new_date = QDateTime().fromString(utc, utc_fmt)
+    if timezone:
+        new_date.setTimeZone(timezone)
+    return new_date
+
+
+def read_data(fname):
+    # Read the CSV content
+    df = pd.read_csv(fname)
+
+    # Remove wrong magnitudes
+    df = df.drop(df[df.mag < 0].index)
+    magnitudes = df["mag"]
+
+    # My local timezone
+    timezone = QTimeZone(b"Europe/Berlin")
+
+    # Get timestamp transformed to our timezone
+    times = df["time"].apply(lambda x: transform_date(x, timezone))
+
+    return times, magnitudes
+
+
+if __name__ == "__main__":
+    options = argparse.ArgumentParser()
+    options.add_argument("-f", "--file", type=str, required=True)
+    args = options.parse_args()
+    data = read_data(args.file)
+
+    # Qt Application
+    app = QApplication(sys.argv)
+
+    widget = Widget(data)
+    window = MainWindow(widget)
+    window.show()
+
+    sys.exit(app.exec())
+
+
+#C:\Users\alexa/AppData/Local/Programs/Python/Python313/python.exe "QT_Tutorial/main.py" -f "QT_Tutorial/all_day.csv" unten im powershell eingeben
+#C:\Users\alexa/AppData/Local/Programs/Python/Python312/python.exe -m pip install pandas
