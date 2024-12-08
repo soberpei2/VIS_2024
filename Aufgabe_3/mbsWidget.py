7869d2e87e50733f7396ac2f1e03f0c24dd7b598
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-08 11:01:20 +0100

    Konstruktor eines leeren Widgets mit Titel

--- Aufgabe_3/mbsWidget.py
+++ Aufgabe_3/mbsWidget.py
@@ -1,10 +1,21 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (QApplication, QLabel, QWidget, QListWidget,
+                               QListWidgetItem, QPushButton, QVBoxLayout, QHBoxLayout)
 
 #================================================================================
 #                               KLASSE - mbsWidget                                 #
 #================================================================================
 
+class mbsWidget(QWidget):
+    # Konstruktor
+    #============
+    def __init__(self, parent = None):
+        # Mutterklassenkonstruktor
+        #-------------------------
+        super(mbsWidget, self).__init__(parent)
 
+        # Fenstertitel definieren
+        self.setWindowTitle("pyFreedyn")
 
+        
