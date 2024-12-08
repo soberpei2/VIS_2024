76c350ccb68619b15d2121a5784853d973eba353
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-08 11:08:18 +0100

    Menüzeile mit Item File hinzu

--- Aufgabe_3/mbsWidget.py
+++ Aufgabe_3/mbsWidget.py
@@ -12,10 +12,18 @@ class mbsWidget(QWidget):
     #============
     def __init__(self, parent = None):
         # Mutterklassenkonstruktor
-        #-------------------------
         super(mbsWidget, self).__init__(parent)
 
         # Fenstertitel definieren
         self.setWindowTitle("pyFreedyn")
 
-        
+        # Anlegen eines Menü-Widgets
+        menu_widget = QListWidget()
+        item = QListWidgetItem("File")
+        menu_widget.addItem(item)
+
+        # Layout des Menü-Widgets
+        menu_layout = QHBoxLayout()
+        menu_layout.addWidget(menu_widget)
+
+        self.setLayout(menu_layout)
