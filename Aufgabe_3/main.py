6042f1d07e66fa3603ba97c0c78cf8c645c93a70
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-08 11:01:42 +0100

    Aufruf aus Kommandozeile (dann anlegen eines Widgets)

--- Aufgabe_3/main.py
+++ Aufgabe_3/main.py
@@ -1,37 +1,22 @@
-# Import benötigter Bibliotheken
-#===============================
-# import sys
-# import random
-# from PySide6 import QtCore, QtWidgets, QtGui
-
-#================================================================================
-#                                       MAIN-FILE                                        #
-#================================================================================
-
+# Importieren benötigter Bibliotheken
+#====================================
 import sys
-from PySide6.QtWidgets import QMainWindow, QApplication
-from vtkmodules.vtkRenderingCore import vtkRenderer
-from vtkmodules.all import vtkConeSource, vtkPolyDataMapper, vtkActor
-import QVTKRenderWindowInteractor as QVTK
+import mbsWidget as mbsW
+from PySide6.QtWidgets import QApplication
 
-QVTKRenderWindowInteractor = QVTK.QVTKRenderWindowInteractor
-def QVTKRenderWidgetConeExample(argv):
-app = QApplication(['QVTKRenderWindowInteractor'])
-window = QMainWindow()
-widget = QVTKRenderWindowInteractor(window)
-window.setCentralWidget(widget)
-ren = vtkRenderer()
-widget.GetRenderWindow().AddRenderer(ren)
-cone = vtkConeSource()
-cone.SetResolution(8)
-coneMapper = vtkPolyDataMapper()
-coneMapper.SetInputConnection(cone.GetOutputPort())
-coneActor = vtkActor()
-coneActor.SetMapper(coneMapper)
-ren.AddActor(coneActor)
-window.show()
-widget.Initialize()
-widget.Start()
-app.exec()
+#================================================================================
+#                                  MAIN-FILE                                    #
+#================================================================================
+
 if __name__ == "__main__":
-QVTKRenderWidgetConeExample(sys.argv)
+    app = QApplication()
+
+    # Widget anlegen und zeigen
+    w = mbsW.mbsWidget()
+    w.show()
+
+    # Ausführen der Anwendung
+    sys.exit(app.exec())
+
+
+
