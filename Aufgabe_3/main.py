538d706b8a04f87051d582ebd2a133c67cc5c60f
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-11 17:00:34 +0100

    Kürzung des main-Files

--- Aufgabe_3/main.py
+++ Aufgabe_3/main.py
@@ -13,27 +13,10 @@ from pathlib import Path
 
 if __name__ == "__main__":
     
-    # Einlesen .fdd-File + Modell erzeugen
-    #=====================================
+    # Abfrage, ob genug Argumente für Aufruf
+    #=======================================
     if len(sys.argv) < 2:
         sys.exit("No fdd file provided! Please run script with additional argument: fdd-filepath!")
-
-    myModel = mbsModel.mbsModel()
-
-    #read fdd file path from input arguments
-    fdd_path = Path(sys.argv[1])
-    myModel.importFddFile(fdd_path)
-    #create path for solver input file (fds)
-    fds_path = fdd_path.with_suffix(".fds")
-    myModel.exportFdsFile(fds_path)
-    #create path for model database file (json)
-    json_path = fdd_path.with_suffix(".json")
-    myModel.saveDatabase(json_path)
-
-    #create new model and load json generated above
-    #(content should be the same)
-    newModel = mbsModel.mbsModel()
-    newModel.loadDatabase(json_path)
     #======================================================================================
 
     # Qt-Anwendung
