f2acfe3d26fc306e16f8bb674fac86c9e9342548
HaHummer <hannes.hummer@students.fh-wels.at>
2024-12-11 12:46:33 +0100

    Aufgabe_2 fertig

--- Aufgabe_2/main.py
+++ Aufgabe_2/main.py
@@ -9,13 +9,18 @@ from vtkmodules.vtkRenderingCore import (
 )
 from vtkmodules.all import vtkInteractorStyleTrackballCamera
 
-if len(sys.argv) < 2:
-    sys.exit("No fdd file provided! Please run script with additional argument: fdd-filepath!")
+#if len(sys.argv) < 2:
+#    sys.exit("No fdd file provided! Please run script with additional argument: fdd-filepath!")
 
 myModel = mbsModel.mbsModel()
 
 #read fdd file path from input arguments
-fdd_path = Path(sys.argv[1])
+#fdd_path = Path(sys.argv[1])
+# Fester Pfad zur fdd-Datei
+fdd_path = Path(r"C:\Users\hanne\Documents\SynologyDrive\Uni\01 Master\3 Semester\Datenaufbereitung und Visualisierung\UE\VIS_2024\VIS_2024\Aufgabe_2\test.fdd")
+if not fdd_path.exists():
+    sys.exit(f"FDD file not found at the specified path: {fdd_path}")
+    
 myModel.importFddFile(fdd_path)
 #create path for solver input file (fds)
 fds_path = fdd_path.with_suffix(".fds")
