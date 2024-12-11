33eb4abc939e2ddc7a0c5343029a041de0e44587
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-11 12:28:13 +0100

    Save und Import-Aktion zu File-Menü hinzu

--- Aufgabe_3/mbsWindow.py
+++ Aufgabe_3/mbsWindow.py
@@ -26,14 +26,30 @@ class mbsWindow(QMainWindow):
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("File")
 
-        # Exit-Aktion definieren (mit QAction)
-        #-------------------------------------
+        # Load-Aktion definieren
+        #-----------------------
+        load_action = QAction("Load", self)
+
+        # Speicher-Aktion defnieren
+        #--------------------------
+        save_action = QAction("Save", self)
+
+        # Import-Aktion definieren
+        #-------------------------
+        importFdd_action = QAction("importFdd", self)
+
+        # Exit-Aktion definieren
+        #-----------------------
         exit_action = QAction("Exit", self)
         exit_action.setShortcut(QKeySequence.Quit)
         exit_action.triggered.connect(self.close)
 
         # Aktionen zu File-Menü hinzufügen
+        self.file_menu.addAction(load_action)
+        self.file_menu.addAction(save_action)
+        self.file_menu.addAction(importFdd_action)
         self.file_menu.addAction(exit_action)
+        
 
         # Statusleiste
         #-------------
