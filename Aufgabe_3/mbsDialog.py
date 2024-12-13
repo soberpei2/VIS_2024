57d8b6fcd60fd706b172e345d6a95a3a789b6be6
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2024-12-13 07:37:38 +0100

    Ok-Button zum Akzeptieren und Close-Button zum Abbrechen

--- Aufgabe_3/mbsDialog.py
+++ Aufgabe_3/mbsDialog.py
@@ -1,6 +1,6 @@
 # Einlesen benötigte Bibliotheken
 #================================
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QDialog,
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QDialog, QHBoxLayout,
                                QVBoxLayout, QLabel, QPushButton, QLineEdit)
 from PySide6.QtGui import QAction, QKeySequence
 
@@ -28,7 +28,15 @@ class mbsDialog(QDialog):
         self.input_field = QLineEdit()
         self.layout().addWidget(self.input_field)
 
-        # Erzeugen einer Schließfläche
-        close_button = QPushButton("Close")
-        close_button.clicked.connect(self.accept)
-        self.layout().addWidget(close_button)
+        # Erzeugen eines Ok- und Close-Buttons nebeneinander
+        button_layout = QHBoxLayout()
+        self.ok_button = QPushButton("OK")
+        self.close_button = QPushButton("Close")
+        button_layout.addWidget(self.ok_button)
+        button_layout.addWidget(self.close_button)
+        self.layout().addLayout(button_layout)
+
+        # Buttonverbindungen
+        self.ok_button.clicked.connect(self.accept)
+        self.close_button.clicked.connect(self.reject)
+        
