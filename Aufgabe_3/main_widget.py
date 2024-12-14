fb9c2f021204bdf00a26bb56e40a52899484e5b0
fpointin <S2310566016@fhooe.at>
2024-12-14 14:14:18 +0100

    noch kompakter

--- Aufgabe_3/main_widget.py
+++ Aufgabe_3/main_widget.py
@@ -1,21 +1,19 @@
-from __future__ import annotations
-
-from PySide6.QtWidgets import (QWidget, QHeaderView, QHBoxLayout, QTableView,
-                               QSizePolicy)
 import vtk
 import QVTKRenderWindowInteractor as QVTK
 
 class Widget(QVTK.QVTKRenderWindowInteractor):
     def __init__(self):
-        """Initialisiert das Widget und den Renderer für das VTK-Renderfenster."""
-        super().__init__()  # Initialisiert den QVTKRenderWindowInteractor
-        self.renderer = vtk.vtkRenderer()  # Erstelle einen VTK-Renderer
-        self.GetRenderWindow().AddRenderer(self.renderer)  # Füge den Renderer dem RenderWindow hinzu
+        super().__init__()
+
+        # Initialisiere Renderer und füge ihn zum RenderWindow hinzu
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(1.0, 1.0, 1.0) # weißer Hintergrund
+        self.GetRenderWindow().AddRenderer(self.renderer)
 
     def update_renderer(self, model):
-        """Aktualisiert den Renderer mit den neuen Modellobjekten."""
-        self.renderer.RemoveAllViewProps()  # Löscht alle bisherigen ViewProps (darunter Modelle)
-        model.showModel(self.renderer)  # Zeigt das neue Modell im Renderer an
-        self.renderer.ResetCamera() # Kamera zurücksetzen, um das gesamte Modell einzufangen
-        self.GetRenderWindow().Render()  # Rendert das Fenster, um das Modell anzuzeigen
+        """Aktualisiert den Renderer mit einem neuen Modell."""
+        #self.renderer.RemoveAllViewProps()  # Entfernt alte Modelle
+        model.showModel(self.renderer)  # Zeigt das neue Modell an
+        self.renderer.ResetCamera()  # Kamera zurücksetzen
+        self.GetRenderWindow().Render()  # Rendern des aktualisierten Fensters
 
--- Aufgabe_3/main_window.py
+++ Aufgabe_3/main_window.py
@@ -1,114 +1,74 @@
-from __future__ import annotations
 from pathlib import Path
 from PySide6.QtGui import QAction, QKeySequence
 from PySide6.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 import mbsModel
 import main_widget as mwid
 
-
 class MainWindow(QMainWindow):
     def __init__(self, widget):
-        QMainWindow.__init__(self)
-        self.setWindowTitle("VTK Integration in QT")
+        super().__init__()
+        self.setWindowTitle("3D Modell in QT mit VTK")
         self.setCentralWidget(widget)
-
-        # MBS Modell initialisieren
-        mbsModel.mbsModel()
-
-        # Menüs erstellen
-        self.menu = self.menuBar()
-        self.file_menu = self.menu.addMenu("File")
-        self.edit_menu = self.menu.addMenu("Edit")
-
-        # Q Actions erstellen
-        load_action = QAction('Load from JSON', self)
-        load_action.triggered.connect(self.load_model)
-
-        save_action = QAction("Save to JSON",self)
-        save_action.triggered.connect(self.save_model)
-
-        openfdd_action = QAction("Open FDD",self)
-        openfdd_action.triggered.connect(self.import_fdd)
-
-        exit_action = QAction("EXIT", self)
-        exit_action.setShortcut(QKeySequence.Quit)
-        exit_action.triggered.connect(self.close)
-
-        # Q Actions zu den Menüs hinzufügen
-        self.file_menu.addAction(load_action)
-        self.file_menu.addAction(save_action)
-        self.file_menu.addAction(openfdd_action)
-        self.file_menu.addAction(exit_action)
-
-        # Status Bar 
-        self.status = self.statusBar()
-        self.status.showMessage("Laden Sie ein JSON oder FDD File ein, um es anzuzeigen")
-
-        # Fenstergröße variabel
-        geometry = self.screen().availableGeometry()
-        self.resize(geometry.width() * 0.8, geometry.height() * 0.7)
-
-        # Hauptfenster konfigurieren
-        self.setWindowTitle("3D Modell in QT mit VTK")  # Setze den Titel des Fensters
         self.setGeometry(100, 100, 800, 600)  # Setze die Fenstergröße und Position
 
-        # VTK-Widget und -Renderer initialisieren
-        self.widget = mwid.Widget()  # Erstelle ein VTK-Widget
-        
-        # Setze den Hintergrund des Renderers auf Weiß
-        self.widget.renderer.SetBackground(1.0, 1.0, 1.0)
+        # Initialisiere MBS Modell
+        self.myModel = mbsModel.mbsModel()
 
-        # Setze das Widget als zentrales Widget des Fensters
-        self.setCentralWidget(self.widget)
+        # Menü und Aktionen erstellen
+        self._create_menus()
 
-        # Initialisiere das RenderWindow, um den Hintergrund anzuzeigen
-        self.widget.GetRenderWindow().Render()  # Rendere das Fenster, um den schwarzen Hintergrund zu sehen
+        # Statusleiste initialisieren
+        self.statusBar().showMessage("Laden Sie ein JSON oder FDD File ein, um es anzuzeigen")
 
+    def _create_menus(self):
+        """Erstellt die Menüs und fügt Aktionen hinzu."""
+        menu_bar = self.menuBar()
+
+        # Datei-Menü
+        file_menu = menu_bar.addMenu("File")
+        file_menu.addAction(self._create_action("Load from JSON", self.load_model))
+        file_menu.addAction(self._create_action("Save to JSON", self.save_model))
+        file_menu.addAction(self._create_action("Open FDD", self.import_fdd))
+        file_menu.addAction(self._create_action("EXIT", self.close, QKeySequence.Quit))
+
+    def _create_action(self, name, method, shortcut=None):
+        """Hilfsmethode zum Erstellen von Aktionen."""
+        action = QAction(name, self)
+        if shortcut:
+            action.setShortcut(shortcut)
+        action.triggered.connect(method)
+        return action
 
     def load_model(self):
         """Lädt ein Modell aus einer JSON-Datei."""
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "Open json File", "", "JSON and FDD Files (*.json *.fdd)", options=options) # Filter gleich nach fdd und json datein
-        
+        filename, _ = QFileDialog.getOpenFileName(self, "Open JSON File", "", "JSON Files (*.json)")
         if filename:
-            if filename.lower().endswith(".json"):  # Überprüfe, ob die Datei eine JSON-Datei ist
-                self.load_json(filename)
+            if filename.lower().endswith(".json"):
+                self.myModel.loadDatabase(Path(filename))
+                self.statusBar().showMessage(f"Modell aus JSON geladen: {filename}")
+                self.centralWidget().update_renderer(self.myModel)
             else:
-                self.messagebox("Ungültiges Dateiformat","Bitte wählen Sie eine JSON-Datei aus.")
+                self._show_message("Ungültiges Dateiformat", "Bitte wählen Sie eine JSON-Datei aus.")
         else:
             self.statusBar().showMessage("Modell-Laden abgebrochen")
-    
-    def load_json(self,filename):
-            self.myModel = mbsModel.mbsModel()  # Erstelle ein neues Modell
-            self.myModel.loadDatabase(Path(filename))  # Lade das Modell aus der JSON-Datei
-            self.statusBar().showMessage(f"Modell aus JSON Datei geladen: {filename}")
-            self.widget.update_renderer(self.myModel)  # Aktualisiere das Rendering mit dem neuen Modell
-        
+
     def save_model(self):
-        """Speichert das Modell in einer JSON-Datei."""
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Save Model File", "", "JSON Files (*.json)", options=options)
+        """Speichert das aktuelle Modell in einer JSON-Datei."""
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Model File", "", "JSON Files (*.json)")
         if filename:
-            self.myModel.saveDatabase(Path(filename))  # Speichert das Modell
+            self.myModel.saveDatabase(Path(filename))
             self.statusBar().showMessage(f"Modell gespeichert: {filename}")
 
     def import_fdd(self):
-        """Importiert ein FDD-Modell aus einer Datei."""
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "Import FDD File", "", "JSON and FDD Files (*.json *.fdd)", options=options) # Filter gleich nach fdd und json datein
-
-        if filename.lower().endswith(".fdd"):  # Überprüfe, ob die Datei eine Fdd-Datei ist
-            self.myModel = mbsModel.mbsModel()
+        """Importiert ein Modell aus einer FDD-Datei."""
+        filename, _ = QFileDialog.getOpenFileName(self, "Import FDD File", "", "FDD Files (*.fdd)")
+        if filename.lower().endswith(".fdd"):
             self.myModel.importFddFile(filename)
             self.statusBar().showMessage(f"FDD-Datei importiert: {filename}")
-            self.widget.update_renderer(self.myModel)
+            self.centralWidget().update_renderer(self.myModel)
         else:
-            self.messagebox("Ungültiges Dateiformat","Bitte wählen Sie eine FDD-Datei aus.")
+            self._show_message("Ungültiges Dateiformat", "Bitte wählen Sie eine FDD-Datei aus.")
 
-    def messagebox(self,title,text):
-            msg_box = QMessageBox(self)
-            msg_box.setIcon(QMessageBox.Critical)
-            msg_box.setWindowTitle(title)
-            msg_box.setText(text)
-            msg_box.exec()
-
+    def _show_message(self, title, text):
+        """Zeigt eine Fehlermeldung an."""
+        QMessageBox.critical(self, title, text)
