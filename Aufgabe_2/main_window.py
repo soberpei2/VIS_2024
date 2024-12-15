ae47d1a645f62c27e18cbfb2505bb0b6c8cc290b
Gugarutz <demidermak@gmail.com>
2024-12-15 12:11:32 +0100

    menu bar implemented

--- Aufgabe_2/main_window.py
+++ Aufgabe_2/main_window.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QMainWindow, QVBoxLayout, QWidget, QMenuBar, QFileDialog, QMessageBox
+)
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from vtkmodules.all import vtkInteractorStyleTrackballCamera
 
@@ -31,3 +33,43 @@ class MainWindow(QMainWindow):
         style = vtkInteractorStyleTrackballCamera()
         self.vtk_interactor.SetInteractorStyle(style)
         self.vtk_interactor.Initialize()
+
+        # Create the menu bar
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        # Menu bar
+        menu_bar = QMenuBar(self)
+        self.setMenuBar(menu_bar)
+
+        # File menu
+        file_menu = menu_bar.addMenu("File")
+
+        # Add actions to the File menu
+        file_menu.addAction("Load", self.load_file)
+        file_menu.addAction("Save", self.save_file)
+        file_menu.addAction("ImportFdd", self.import_fdd)
+        file_menu.addSeparator()
+        file_menu.addAction("Exit", self.close_app)
+
+    # Placeholder actions
+    def load_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Load File", "", "All Files (*.*)")
+        if file_name:
+            QMessageBox.information(self, "Load File", f"Loaded file: {file_name}")
+            # TODO: Add code to load the file into your model
+
+    def save_file(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save File", "", "All Files (*.*)")
+        if file_name:
+            QMessageBox.information(self, "Save File", f"Saved file: {file_name}")
+            # TODO: Add code to save your model data
+
+    def import_fdd(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Import Fdd File", "", "FDD Files (*.fdd)")
+        if file_name:
+            QMessageBox.information(self, "Import Fdd", f"Imported FDD file: {file_name}")
+            # TODO: Add code to process the imported FDD file
+
+    def close_app(self):
+        self.close()
